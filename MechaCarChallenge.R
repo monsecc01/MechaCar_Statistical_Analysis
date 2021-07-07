8ef9c04d5bf1a528451a03e103129812b6ecc44e
monsecc01 <81447450+monsecc01@users.noreply.github.com>
2021-07-06 21:32:58 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-## Deliverable 1. Linear Regression to Predict MPG ##
+## DELIVERABLE 1. Linear Regression to Predict MPG ##
 
 # Technical Analysis
 mechaCar <- read.csv(file = "MechaCar_mpg.csv", check.names = F, stringsAsFactors = F)
@@ -10,4 +10,12 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechaCar))
 # Adjusted R-squared:  0.6825
 # p-value: 5.35e-11
+
+## DELIVERABLE 2. Summary Stats on Suspension Coils ##
+# summary data frame
+suspensionCoil <- read.csv(file = "Suspension_Coil.csv", check.names = F, stringsAsFactors = F)
+total_summary <- suspensionCoil %>% summarize(mean=mean(PSI), median=median(PSI), variance=var(PSI), sd=sd(PSI))
+
+#lot summary data frame
+lot_summary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(mean=mean(PSI), median=median(PSI), variance=var(PSI), sd=sd(PSI))
 
